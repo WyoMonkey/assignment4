4d95ecd08557c98f5ad9afa62dd857cb26855a61
Lawrence Beck <sonny1388@msn.com>
2017-06-12 00:05:52 -0600

    assignment 4

---
+++ hw assignment4/assignment4.js
@@ -0,0 +1,10 @@
+document.getElementById("nickname").innerHTML = "Wyomonkey";
+document.getElementById("favorites").innerHTML = "coding and food!";
+document.getElementById("hometown").innerHTML =  "Riverton, WY";
+document.body.style.backgroundColor="red";
+var x = document.getElementsByTagName("H1");
+for (i = 0; i<x.length; i++){
+ x[i].style.backgroundColor = "blue";
+}
+document.body.style.fontFamily = "sans-serif";
+var y = document.getElementsByTagName("LI");
